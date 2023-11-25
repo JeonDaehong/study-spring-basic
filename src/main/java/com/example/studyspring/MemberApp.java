5c44880d9e3b32718fb20b171ba39e4decde8334
JeonDaehong <daehong770@gmail.com>
2023-11-25 15:44:49 +0900

    study-spring : Spring @Configuration 과 @Bean 에 대한 공부

--- src/main/java/com/example/studyspring/MemberApp.java
+++ src/main/java/com/example/studyspring/MemberApp.java
@@ -4,12 +4,16 @@ import com.example.studyspring.member.Grade;
 import com.example.studyspring.member.Member;
 import com.example.studyspring.member.MemberService;
 import com.example.studyspring.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "홍길동", Grade.VIP);
         memberService.join(member);
